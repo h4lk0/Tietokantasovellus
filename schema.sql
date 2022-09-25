b942236bc403177081ad07c214b6739adad5d008
h4lk0 <jenni.e.halkola@helsinki.fi>
2022-09-25 20:30:54 +0300

    Update schema.sql

---
+++ schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE shelters (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    info TEXT,
+    in_storage BOOLEAN,
+    visible BOOLEAN
+);
+
+CREATE TABLE tools (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    info TEXT,
+    amount INTEGER,
+    in_storage BOOLEAN,
+    visible BOOLEAN
+);
+
+CREATE TABLE misc (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    info TEXT,
+    amount INTEGER,
+    in_storage BOOLEAN,
+    visible BOOLEAN
+);
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT,
+    is_admin BOOLEAN
+);
+
+CREATE TABLE loans (
+    username REFERENCES users(username),
+    name TEXT,
+    amount INTEGER
+);
