2b8aa0b45e7cd8b7e7264c3190cf26bca5c671d9
h4lk0 <jenni.e.halkola@helsinki.fi>
2022-09-25 21:08:42 +0300

    Minor fixes

--- users.py
+++ users.py
@@ -1,3 +1,12 @@
 from db import db
 from flask import session
-from werkzeug.security import 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+def login(username, password):
+    pass
+
+def logout(username):
+    pass
+
+def register(username, password):
+    pass
