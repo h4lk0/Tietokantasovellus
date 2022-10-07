a73915ef793d5a39f56ce37b3e49b19fe94c8513
h4lk0 <jenni.e.halkola@helsinki.fi>
2022-10-07 14:22:35 +0300

    Add methods

--- storage.py
+++ storage.py
@@ -1,3 +1,4 @@
+from unittest import result
 from db import db
 
 def get_list():
@@ -5,3 +6,15 @@ def get_list():
     result = db.session.execute(sql)
     return result.fetchall()
 
+def get_loans(username):
+    sql = "SELECT name, amount FROM loans WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    return result.fetchall()
+
+def get_all():
+    sql = "SELECT username, name, amount FROM loans"
+    result = db.session.execute(sql)
+    loans = result.fetchall()
+    if not loans:
+        loans.insert(0, ("Ei lainoja", "tällä hetkellä", 0))
+    return loans
